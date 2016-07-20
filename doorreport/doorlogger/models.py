2eb1799b2d02e25baa5918f0b038d0f02f3eada8
dwmodem <dwmodem@gmail.com>
2016-07-19 21:24:20 -0400

    Fix for broken view, work on models

--- doorreport/doorlogger/models.py
+++ doorreport/doorlogger/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime 
 
-# Create your models here.
+class Door(models.Model):
+	door_name = models.CharField(max_length=200)
+
+class Event(models.Model):
+	door = models.ForeignKey(Door, on_delete=models.CASCADE)
+	event_type = models.CharField(max_length=20)
+	open_level = models.IntegerField(default=0)
+	time_stamp = models.DateTimeField(default=datetime.now, blank=True)
