745af4d14ced8f8dcb9697600d7c97a9fc65d39f
dwmodem <dwmodem@gmail.com>
2016-07-31 20:44:32 -0400

    Got views working for existing doors/events

--- doorreport/doorlogger/models.py
+++ doorreport/doorlogger/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from datetime import datetime 
 
 class Door(models.Model):
-	door_name = models.CharField(max_length=200)
+	door_name = models.CharField(max_length=144)
+	door_type = models.CharField(max_length=35)
+
+	def __str__(self):
+		return self.door_name
 
 class Event(models.Model):
 	door = models.ForeignKey(Door, on_delete=models.CASCADE)
 	event_type = models.CharField(max_length=20)
 	open_level = models.IntegerField(default=0)
-	time_stamp = models.DateTimeField(default=datetime.now, blank=True)
+	time_stamp = models.DateTimeField('time stamp')
+	
+	def __str__(self):
+		
+		return ("%s: %d" % (self.event_type, self.open_level)) 
--- doorreport/doorlogger/urls.py
+++ doorreport/doorlogger/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
-
+from django.views.generic import TemplateView
 from . import views
 
+app_name = 'doorlogger'
 urlpatterns = [
-    url(r'^index', views.index, name='index'),
-    url(r'^$', views.index, name='index'),
-]
+    
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^logevent$', views.logevent, name='logevent'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^.*$', views.IndexView.as_view(), name='index'),
+    
+    ]
+
